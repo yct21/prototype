3b471a58597da2bd78c2232edaafa5dddd607947
yct21 <yct21@12tcy.com>
2022-04-20 23:29:25 +0800

    nix flake init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "Personal configurations for my Mac and NixOS";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
+
+  };
+}
